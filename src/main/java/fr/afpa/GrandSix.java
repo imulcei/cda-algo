869d8314d073aff178e20fa06fc6145ea0398e0b
imulcei <lucie.fiorrito@proton.me>
2025-04-14 12:03:23 +0200

    wip array2D

--- src/main/java/fr/afpa/GrandSix.java
+++ src/main/java/fr/afpa/GrandSix.java
@@ -6,11 +6,38 @@ public class GrandSix {
 
     }
 
+    /**
+     * 
+     * @param array2D Un tableau 2D d'entiers
+     * @return Renvoie la somme des entiers du tableau 2D
+     */
     public static int sumArray(int[][] array2D) {
         int sum = 0;
         for (int i = 0; i < array2D.length; i++) {
             for (int j = 0; j < array2D[i].length; j++) {
                 sum += array2D[i][j];
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * 
+     * @param array2D      Tableau d'entiers 2D
+     * @param width        Nombre de colonnes dans le tableau
+     * @param height       Nombre de lignes dans le tableau
+     * @param diagonalType Indique qu'il faut calculer la diagonale 1 ou 2
+     * @return Renvoie la somme des entiers du tableau 2D composant la diagonale
+     */
+    public static int sumDiagonalArray(int[][] array2D, int width, int height, int diagonalType) {
+        int sum = 0;
+        if (width == height) {
+            for (int i = 0; i < height; i++) {
+                if (diagonalType == 1) {
+                    sum += array2D[i][i];
+                } else if (diagonalType == 2) {
+                    sum += array2D[i][width - 1 - i];
+                }
             }
         }
         return sum;
--- src/test/java/fr/afpa/GrandSixTest.java
+++ src/test/java/fr/afpa/GrandSixTest.java
@@ -10,4 +10,14 @@ public class GrandSixTest {
         int result = GrandSix.sumArray(array2D);
         System.out.println(result);
     }
+
+    @Test
+    public void sumDiagonalArrayTest() {
+        int[][] array2D = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int result = GrandSix.sumDiagonalArray(array2D, 3, 3, 1);
+        System.out.println(result);
+
+        int result2 = GrandSix.sumDiagonalArray(array2D, 3, 3, 2);
+        System.out.println(result2);
+    }
 }
