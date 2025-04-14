883e23d390b2b959dc0876163073bcbe54d80270
imulcei <lucie.fiorrito@proton.me>
2025-04-14 16:54:54 +0200

    wip

---
+++ src/main/java/fr/afpa/GrandNeuf.java
@@ -0,0 +1,11 @@
+package fr.afpa;
+
+public class GrandNeuf {
+
+    public static int countVoyelles(String str) {
+        char[] voyelles = { 'a', 'e', 'i', 'o', 'u', 'y' };
+        int numberOfVoyelles = 0;
+        return numberOfVoyelles;
+    }
+
+}
