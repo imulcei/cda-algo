6b21b9b3cc5a383d94c820b6bcc2d75efc47fe28
imulcei <lucie.fiorrito@proton.me>
2025-04-15 14:08:47 +0200

    level 2 wip

--- src/main/java/fr/afpa/GrandNeuf.java
+++ src/main/java/fr/afpa/GrandNeuf.java
@@ -48,4 +48,55 @@ public class GrandNeuf {
         return result;
     }
 
+    /**
+     * 
+     * @param str Phrase entrée par l'utilisateur
+     * @return Renvoie le mirroir de la phrase
+     */
+    public static String strMirror(String str) {
+        String newStr = "";
+        for (int i = str.length() - 1; i > -1; i--) {
+            char character = str.charAt(i);
+            newStr += character;
+        }
+        return newStr;
+    }
+
+    /**
+     * 
+     * @param str Phrase entrée par l'utilisateur
+     * @return Renvoie la phrase en camel case
+     */
+    public static String strToCamelCase(String str) {
+        // sépare les mots où il y a des espaces
+        String[] words = str.split("[\\W_]");
+        StringBuilder modifiedStr = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (i == 0) {
+                // premier mot en minuscule
+                modifiedStr.append(Character.toLowerCase(word.charAt(0)));
+                modifiedStr.append(word.substring(1));
+            } else {
+                // tous les mots suivants ont leurs 1e lettres en majuscule
+                modifiedStr.append(Character.toUpperCase(word.charAt(0)));
+                modifiedStr.append(word.substring(1));
+            }
+
+        }
+        return modifiedStr.toString();
+    }
+
+    /**
+     * 
+     * @param str    Phrase entrée par l'utilisateur
+     * @param subStr Sous-chaine à localiser
+     * @return Renvoie les indexes du 1e et dernier caractère
+     */
+    public static int[] locateString(String str, String subStr) {
+        int indexStart = str.indexOf(subStr);
+        int indexEnd = str.lastIndexOf(subStr);
+        int[] result = { indexStart, indexEnd };
+        return result;
+    }
 }
--- src/test/java/fr/afpa/GrandNeufTest.java
+++ src/test/java/fr/afpa/GrandNeufTest.java
@@ -1,5 +1,7 @@
 package fr.afpa;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 public class GrandNeufTest {
@@ -20,5 +22,29 @@ public class GrandNeufTest {
     public void strToUppercaseTest() {
         String result = GrandNeuf.strToUppercase("AaAh oOoh EeEh");
         System.out.println(result);
+    }
+
+    @Test
+    public void strMirrorTest() {
+        String result = GrandNeuf.strMirror("J'aime les lasagnes");
+        System.out.println(result);
+    }
+
+    @Test
+    public void strToCamelCaseTest() {
+        String result = GrandNeuf.strToCamelCase("J'aime les lasagnes");
+        System.out.println(result);
+
+        String result2 = GrandNeuf.strToCamelCase("Maman aime le poulet");
+        System.out.println(result2);
+    }
+
+    @Test
+    public void locateStringTest() {
+        int[] result = GrandNeuf.locateString("J'aime les lasagnes", "les");
+        System.out.println(Arrays.toString(result));
+
+        int[] result2 = GrandNeuf.locateString("J'aime les lasagnes", "aime");
+        System.out.println(Arrays.toString(result2));
     }
 }
