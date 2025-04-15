e522b415ee9bc089058cec9b7421e91c05bc30ea
imulcei <lucie.fiorrito@proton.me>
2025-04-15 09:56:32 +0200

    level 2

--- src/main/java/fr/afpa/GrandNeuf.java
+++ src/main/java/fr/afpa/GrandNeuf.java
@@ -5,6 +5,15 @@ public class GrandNeuf {
     public static int countVoyelles(String str) {
         char[] voyelles = { 'a', 'e', 'i', 'o', 'u', 'y' };
         int numberOfVoyelles = 0;
+        String lowerStr = str.toLowerCase();
+        for (int i = 0; i < lowerStr.length(); i++) {
+            for (int j = 0; j < voyelles.length; j++) {
+                if (lowerStr.charAt(i) == voyelles[j]) {
+                    numberOfVoyelles += 1;
+                    System.out.println(lowerStr.charAt(i));
+                }
+            }
+        }
         return numberOfVoyelles;
     }
 
--- src/test/java/fr/afpa/GrandNeufTest.java
+++ src/test/java/fr/afpa/GrandNeufTest.java
@@ -1,5 +1,12 @@
 package fr.afpa;
 
+import org.junit.jupiter.api.Test;
+
 public class GrandNeufTest {
 
+    @Test
+    public void countVoyellesTest() {
+        int result = GrandNeuf.countVoyelles("Aaah oooh eeeh");
+        System.out.println(result);
+    }
 }
