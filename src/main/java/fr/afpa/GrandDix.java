b1a8977370c184016d42a0f2367546efdad74345
imulcei <lucie.fiorrito@proton.me>
2025-04-16 10:48:27 +0200

    exos binaires

--- src/main/java/fr/afpa/GrandDix.java
+++ src/main/java/fr/afpa/GrandDix.java
@@ -2,12 +2,30 @@ package fr.afpa;
 
 public class GrandDix {
 
+    /**
+     * 
+     * @param str Chaîne de caractères du nombre binaire à convertir
+     * @return Renvoie le nombre en base 10
+     */
     public static int conversionBaseDix(String str) {
-        int result = 0;
-        for (int i = 0; i < str.length(); i++) {
-            int chiffre = Character.getNumericValue(str.charAt(i));
-            int x = chiffre * 10;
-            System.out.println(chiffre);
+        int result = Integer.parseInt(str, 2);
+        return result;
+    }
+
+    /**
+     * 
+     * @param str Chaîne de caractère du nombre binaire à convertir
+     * @return Renvoie une chaîne de caractères du nombre binaire
+     */
+    public static String conversionBinaire(int number) {
+        String result = "";
+        if (number == 0) {
+            return "0";
+        }
+        while (number > 0) {
+            int modulo = number % 2;
+            result = modulo + result;
+            number = number / 2;
         }
         return result;
     }
--- src/test/java/fr/afpa/GrandDixTest.java
+++ src/test/java/fr/afpa/GrandDixTest.java
@@ -8,5 +8,17 @@ public class GrandDixTest {
     public void conversionBaseDixTest() {
         int res = GrandDix.conversionBaseDix("0110");
         System.out.println(res);
-    } 
+
+        int res1 = GrandDix.conversionBaseDix("1101");
+        System.out.println(res1);
+
+        int res2 = GrandDix.conversionBaseDix("0011");
+        System.out.println(res2);
+    }
+
+    @Test
+    public void conversionBinaireTest() {
+        String res = GrandDix.conversionBinaire(0);
+        System.out.println(res);
+    }
 }
